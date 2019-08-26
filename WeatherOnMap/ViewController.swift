ba9eb5dd399572e87a512d7f54a8284eef46d090
VVSKR <vova.skri.dev@gmail.com>
2019-08-26 11:31:32 +0300

    Initial Commit

---
+++ WeatherOnMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherOnMap
+//
+//  Created by Vova SKR on 26/08/2019.
+//  Copyright © 2019 Vova SKR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
