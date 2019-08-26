54b3d819701476fcd49550a32d3c245ab6ea9acc
VVSKR <vova.skri.dev@gmail.com>
2019-08-26 13:23:13 +0300

    Model done

--- WeatherOnMap/ViewController.swift
+++ WeatherOnMap/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import MapKit
+
 
 class ViewController: UIViewController {
-
+    
+    let initialLocation = CLLocation(latitude: 55.7522200, longitude: 37.6155600)
+    let regionRadius: CLLocationDistance = 3000
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       centerMapOnLocation(location: initialLocation)
+        
+//        mapView.addAnnotation()
     }
-
-
+    
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
+                                                  latitudinalMeters: regionRadius,
+                                                  longitudinalMeters: regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    
 }
 
